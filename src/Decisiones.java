29deb60b47443a61c75a1cbb3052897a3886df18
developerharo <developerharo@gmail.com>
2025-04-07 23:33:20 -0600

    Modificando Decisiones.java (Uso de operadores lógicos en if - else)

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -3,11 +3,19 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
+        String tipoDePlan = "plus";
 
         if (fechaDeLanzamiento >= 2022) {
             System.out.println("Películas más populares");
         } else {
-            System.out.printf("Película retro que vale la pena ver");
+            System.out.println("Película retro que vale la pena ver");
+        }
+
+        if (incluidoEnElPlan && tipoDePlan.equals("plus")) {
+            System.out.println("Disfrute de su película");
+
+        } else {
+            System.out.println("Película no disponible para su plan actual");
         }
 
     }
